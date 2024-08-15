738c42020f7ee44e0063c5df52d7eac2413ad070
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-08-15 05:40:35 -0700

    add llama3.1 script

---
+++ scripts/examples/eval_llama_3.1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+save_dir="eval_results/"
+global_record_file="eval_results/eval_record_collection.csv"
+model="/mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints/tulu_lora_sft_default_template_8b"
+selected_subjects="all"
+gpu_util=0.8
+
+# cd ../../
+export CUDA_VISIBLE_DEVICES=0
+
+python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util
+
